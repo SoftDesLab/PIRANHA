0f4188df76c99dc8df2ec68d69119c6e5a3a23a1
OmarJulkipli <60902542+OmarJulkipli@users.noreply.github.com>
2020-04-29 00:36:09 +0800

    Add files via upload

--- Application Layer(Logic of the program)/dbmanager.py
+++ Application Layer(Logic of the program)/dbmanager.py
@@ -5,56 +5,21 @@ import matplotlib.dates as mdates
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
-def __init__(self):
-	pass
-	
-def database(self):
-	connection = sqlite3.connect("urlData.db")
-	return connection
+class dbmanager(object):
+	def database(self):
+		connection = sqlite3.connect("urlData.db")
+		return connection
 
-def inputdb(insert):
-	data = insert
-	input = database().cursor()
-	input.execute("INSERT INTO URL VALUES(?,?)",(data, 0))
+	def inputdb(self, insert):
+		connection = sqlite3.connect("urlData.db")
+		data = insert
+		bad = 0
+		input = connection.cursor()
+		input.execute("INSERT INTO URL (URLs, Condition) VALUES (?, ?)",(data, bad))
+		connection.commit()
+		connection.close()
 
-def graphdb(self):
-	pass
-
-# insert = input("Insert URLs:")
-# inputdb(insert)
-# test_percentage = .2
-# conn = sqlite3.connect('urlData.db')
-# query = "SELECT Condition FROM URL"
-# TM = pd.read_sql(query, conn)
-# sns.countplot(x="Condition", y="URLs", data=TM)
-# TM1 = TM.head(100)
-# # plt.scatter(TM1['URLs'], TM1['Condition'])
-# plt.xlabel("QUANTITY", fontsize=16)
-# plt.ylabel("STATUS", fontsize = 16)
-# plt.title("Phishing Website Detection", fontsize =25)
-# plt.show()
-
-# dataset = database()
-# test_df = train_test_split(dataset, test_size=test_percentage, random_state=42)
-# count_train_class = pd.valuecounts(train_df['Class'])
-# count_train_class.plot(kind='bar, fontsize=16')
-# plt.title("Website Extraction", fontsize=16)
-# plt.xticks(rotation='horizontal')
-# plt.xlabel("Class", fontsize=20)
-# plt.ylabel("Class Count", fontsize=20)
-# plt.show()
-
-# connection = sqlite3.connect("urlData.db")
-# # query = "SELECT URLs FROM URL WHERE URL.Condition='0'"
-# # result = connection.execute(query)
-# c = connection.cursor()
-
-# def graph_data():
-# 	c.execute('SELECT Condition FROM URL')
-
-# # "SELECT URLs FROM URL WHERE URL.Condition='0'
-
-# insert = (input("ENTER URL:"))
-# input = database().cursor()
-# input.execute("INSERT INTO URL VALUES(" + insert + ", 0)")
-
+	def graphdb(self):
+		query = "SELECT COUNT(Condition), Condition from URL group by Condition"
+		TM = pd.read_sql(query,database(self))
+		
