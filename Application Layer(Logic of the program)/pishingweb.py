571cb734d92274d9e10b08dd30e76605ee3b19d8
OmarJulkipli <60902542+OmarJulkipli@users.noreply.github.com>
2020-04-28 19:13:06 +0800

    Add files via upload

--- Application Layer(Logic of the program)/pishingweb.py
+++ Application Layer(Logic of the program)/pishingweb.py
@@ -5,22 +5,20 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import dbmanager
 
-class Pishingweb:
+class Pishingweb(object):
 
     def __init__(self):
         super().__init__()
-        cnx = sqlite3.connect('urlData.db')
-        self.urlData = pd.read_sql_query("SELECT * FROM Url", cnx)
+        self.urlData = pd.read_sql_query("SELECT * FROM Url", dbmanager.database(self))
 
-
-    #hihimayin lang nento yung mga strings 
     def makeTokens(self, f):
         tkns_BySlash = str(f.encode('utf-8')).split('/')
         total_Tokens = []
         for i in tkns_BySlash:
             tokens = str(i).split('-')
-            tkns_ByDot = [] #list of words 
+            tkns_ByDot = []
             for j in range(0,len(tokens)):
                 temp_Tokens = str(tokens[j]).split('.')
                 tkns_ByDot = tkns_ByDot + temp_Tokens
@@ -28,34 +26,21 @@ class Pishingweb:
         total_Tokens = list(set(total_Tokens))
         if "com" in total_Tokens:
             total_Tokens.remove('com')
+        return total_Tokens
 
+    def prediction(self, urlInput):
+        y = self.urlData["Condition"] 
+        urlList = self.urlData["URLs"] 
 
-    def accuracy(self):
-        y = self.urlData["Condition"] #kukunin niya yung label na good/bad sa urldata.csv
-        urlList = self.urlData["URLs"] #kukuinin niya lahat ng mga website either good or bad
-
-#dito na papasok yung machine learning gagamit ng regression para malaman kung good or bad
         vector = TfidfVectorizer(tokenizer=self.makeTokens)
         x = vector.fit_transform(urlList)
         xLR, xTest, yLR, yTest = train_test_split(x, y, test_size=0.2, random_state=42)
         regression = LogisticRegression()
         regression.fit(xLR, yLR)
-        print("Accuracy ", regression.score(xTest, yTest))#print yung accuracy ng makukuha niya 
+        print("Accuracy ", regression.score(xTest, yTest) * 100)
 
-        urlInput = "www.google.com"
-
-        #x_pred = ["online.bpi.com.ph/portalserver/onlinebanking/sign-in","google.com","facebook.com/login","sanagustinturismo.co/Facebook/", "facebook.pcriot.com/login.php"]
         url = urlInput.strip().split()
-    
         urlTok = vector.transform(url)
-        condition = regression.predict(urlTok) #ito na yung mga label kung bad/good. 
-        if condition == [1]:
-            
-            return condition
-        else:
-            
-            return condition
-        
+        value = regression.predict(urlTok) 
+        return value
 
-        #created P.I.R.A.N.H.A.
-        """cre:nathan"""
